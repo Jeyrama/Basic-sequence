d609599c7ad0b613c8451a6d95918adbc21d73df
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-05 18:10:16 -0700

    Update Basic-sequence.js

--- Basic-sequence.js
+++ Basic-sequence.js
@@ -22,3 +22,13 @@ Examples:
 
 
 // Solution
+
+function sumOfN(n) {
+  var array = [];
+  array[0] = 0;
+  for (var i = 1; i < Math.abs(n)+1; i++) {
+    if (n > 0) array[i] = array[i-1]+i;
+    else array[i] = array[i-1]-i;
+  }
+  return array;
+};
